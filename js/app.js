ff108b4e062688bc91050ebf776bfe10ec40d3cc
JRAMOSROCHA <JRAMOSROCHA@HOTMAIL.COM>
2024-08-02 19:27:42 -0300

    PROJETO FINAL

--- js/app.js
+++ js/app.js
@@ -1,16 +1,25 @@
+let quantidadeJogosAlugados=1;
+
 function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`);
     let imagem = gameClicado.querySelector('.dashboard__item__img');
     let botao = gameClicado.querySelector('.dashboard__item__button');
-   
+    
     if(imagem.classList.contains('dashboard__item__img--rented')){
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
+        decisao = confirm('Você deseja realmente devolver este item?');
+        if(decisao==true){
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            quantidadeJogosAlugados--;
+        }
     }
     else{
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
+        quantidadeJogosAlugados++;
     }
+    console.log(`A quantidade de jogos alugados é de: ${quantidadeJogosAlugados}`);
+    alert(`A quantidade de jogos alugados é de: ${quantidadeJogosAlugados}`);
 }
